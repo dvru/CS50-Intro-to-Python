5c4a9e69507bf2a72112be9b4944f497b2668673
Damini Varu <50891247+dvru@users.noreply.github.com>
2023-08-29 20:49:52 -0500

    added all the methods on one line

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 # Ask user for their name and Removes whitespace from str and capilative user's name
 name = input("What's your name? ").strip().title()
 
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
 # Say hello to user
 print(f"hello, {name}")
 
